d476072dba6748df3d82628bec54105b020d1881
Yash Khadilkar <153229672+yashkhadilkar@users.noreply.github.com>
2025-11-13 09:04:27 -0800

    Create Exchange_Seats.sql

---
+++ Subqueries/Exchange_Seats.sql
@@ -0,0 +1,49 @@
+'''
+Table: Seat
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+The ID sequence always starts from 1 and increments continuously.
+ 
+
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+
+Return the result table ordered by id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Seat table:
++----+---------+
+| id | student |
++----+---------+
+| 1  | Abbot   |
+| 2  | Doris   |
+| 3  | Emerson |
+| 4  | Green   |
+| 5  | Jeames  |
++----+---------+
+Output: 
++----+---------+
+| id | student |
++----+---------+
+| 1  | Doris   |
+| 2  | Abbot   |
+| 3  | Green   |
+| 4  | Emerson |
+| 5  | Jeames  |
++----+---------+'''
+
+SELECT IF(id <> (SELECT MAX(id) FROM Seat), IF(id % 2 = 0, id - 1, id + 1), IF(id % 2 = 0, id - 1, id)) AS id, student
+FROM Seat
+ORDER BY id
