8dfdd251d1afec0ae55af6a0fcb3345bf96d40f9
Yash Khadilkar <153229672+yashkhadilkar@users.noreply.github.com>
2025-11-17 10:52:16 -0800

    Update Average_Time_of_Process_per_Machine.sql

--- Basic_Joins/Average_Time_of_Process_per_Machine.sql
+++ Basic_Joins/Average_Time_of_Process_per_Machine.sql
@@ -68,10 +68,10 @@ Machine 2s average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
 
 '''
 
-SELECT a1.machine_id, ROUND((AVG(a1.timestamp - a2.timestamp) * -1) , 3) AS processing_time
+SELECT a1.machine_id, ROUND(AVG(a1.timestamp - a2.timestamp) , 3) AS processing_time
 FROM Activity a1
 JOIN Activity a2 
 ON a1.machine_id = a2.machine_id 
 AND a1.process_id = a2.process_id 
-WHERE a1.activity_type = "start" AND a2.activity_type = "end"
+WHERE a1.activity_type = "end" AND a2.activity_type = "start"
 GROUP BY a1.machine_id
